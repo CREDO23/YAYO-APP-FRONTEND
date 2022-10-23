b49872d3851748b4eba50c02bbb822e932f6329a
CREDO23 <thierrybakera12@gmail.com>
2022-10-23 09:20:35 +0200

    setup(core) : husky , prettier & eslint

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+            const [count, setCount] = useState(0);
+            const [crdo , setcredo]= useState()
 
     return (
         <div className="App">
