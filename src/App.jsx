fe962bfb96afa1bf233bab08f26bff584925f343
CREDO23 <thierrybakera12@gmail.com>
2022-10-26 08:22:05 +0200

    setup : talwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-
-
 function App() {
-
     return (
         <div className=" h-[100vh] bg-blue-400 flex items-center justify-center ">
-            <p className=' text-orange-100'>Hello word</p>
+            <p className=" text-orange-100">Hello word</p>
         </div>
     );
 }
