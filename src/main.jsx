5f539a4c75876f6262cbd768b7e6692473dfecc8
CREDO23 <thierrybakera12@gmail.com>
2022-11-01 17:04:27 +0200

    refact : ..

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './main.css';
-import { Provider } from 'react-redux/es/exports'
-import store from './store'
+import { Provider } from 'react-redux/es/exports';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-        
     </React.StrictMode>,
 );
